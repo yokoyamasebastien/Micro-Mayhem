b9bd09edc1436255153eca07c25694aafcce47fb
yokoyamasebastien <syokoyama2001@nevada.unr.edu>
2022-05-08 01:17:55 -0700

    Created Jump Height Increase Upgrade

---
+++ Micro Mayhem/Assets/Micro Mayhem/Scripts/Consumables/JumpIncrease.cs
@@ -0,0 +1,33 @@
+// NAME: Sebastien Yokoyama
+// EMAIL: syokoyama2001@nevada.unr.edu
+// COURSE: CS 381.1001
+// ASSIGNMENT: Semester Project
+// FILE NAME: JumpIncrease.cs
+/* FILE DESCRIPTION: Allows an object to be consumed by the player upon contact.
+ The item will increase the jump height of the player by 15%. */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpIncrease : Consumable
+{
+    /*---------- Properties ----------*/
+    float percentage = 0.15f;
+
+    /*---------- Methods ----------*/
+    /* Collect Method
+     * Increases jump height of player, destroys gameobject on consumption
+     * Removes consumable from active consumables list
+     */
+    public override void Collect()
+    {
+        PlayerMgr.inst.jumpHeight *= (1 + percentage);
+
+        //if (collectSound)
+        //AudioSource.PlayClipAtPoint(collectSound, transform.position
+        //collectEffect.Play();
+
+        base.Collect(); // Delete this object
+    }
+}
